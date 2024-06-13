743a238b548430f24b49f55ad8a57428d49b04ab
Max Kosheutov <maxkosh1994@cmail.com>
2024-06-13 22:27:42 +0300

    fix issue #1

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 console.log('My application');
+console.log('ну ты француз бомбомбом');
--- lection.js
+++ lection.js
@@ -1,3 +1,10 @@
 //TODO Шаг 1: инициализация git-репозитория
 //NOTE Шаг 2: проверить состояние через git status
 //TODO Шаг 3: попробовать закоммитить git commit -m 'initial commit'
+//NOTE Шаг 4: git add ./
+//TODO Шаг 5: коммит git commit -m 'initial commit'
+//TODO Шаг 6: git log
+//TODO Шаг 7: .gitignore + файл не для гита
+//TODO Шаг 8: создание удаленного репозитория
+//TODO Шаг 9: git remote add origin git@github.com:MaxKosh1994/myCustom.git
+//TODO Шаг 9: git push -u origin master
